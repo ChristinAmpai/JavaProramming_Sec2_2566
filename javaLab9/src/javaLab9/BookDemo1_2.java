ee7faf8351f011ab232b5ba1df8ce9cee4cab4b7
ChristinAmpai <ampaichristin70@gmail.com>
2024-01-17 13:18:49 +0700

    LabSheet09

--- javaLab9/src/javaLab9/BookDemo1_2.java
+++ javaLab9/src/javaLab9/BookDemo1_2.java
@@ -1,11 +1,11 @@
 package javaLab9;
-import java.util.*;
+import java.util.Scanner;
 public class BookDemo1_2 {
-	static Scanner scan = new Scanner(System.in);
+	 
 	public static void main(String[] args) {
-		       
+		       Scanner scan = new Scanner(System.in);
 			   System.out.print("Input book title:");
-		       String title = scan.next();
+		       String title = scan.nextLine();
 		       System.out.print("Input book price:");
 		       float price = scan.nextFloat();
 		       System.out.print("Input publish year:");
